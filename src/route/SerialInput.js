89eae999aaf55950c0d2e8da9204d9203d73e7d6
Sanmountain <tksgk7777@naver.com>
2023-04-19 11:56:49 +0900

    230419 롯데추가, 로젠 초기값 설정 완료

--- src/route/SerialInput.js
+++ src/route/SerialInput.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import '../styles/SerialInput.css';
-import { serialInsert } from '../api/API';
+import React, { useState } from "react";
+import "../styles/SerialInput.css";
+import { serialInsert } from "../api/API";
 
 function SerialInput() {
-  const [devSerial, setDevSerial] = useState('');
-  const [macAddr, setMacAddr] = useState('');
-  const [branCd, setBranCd] = useState('');
-  const [company, setCompany] = useState('');
+  const [devSerial, setDevSerial] = useState("");
+  const [macAddr, setMacAddr] = useState("");
+  const [branCd, setBranCd] = useState("");
+  const [company, setCompany] = useState("logen");
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -15,17 +15,17 @@ function SerialInput() {
         dev_serial: devSerial,
         mac_addr: macAddr,
         bran_cd: branCd,
-        company: company
+        company: company,
       });
       console.log(response.data);
-      alert('시리얼 등록이 완료되었습니다.');
-      setDevSerial('');
-      setMacAddr('');
-      setBranCd('');
-      setCompany('');
+      alert("시리얼 등록이 완료되었습니다.");
+      setDevSerial("");
+      setMacAddr("");
+      setBranCd("");
+      setCompany("");
     } catch (error) {
       console.error(error);
-      alert('시리얼 등록에 실패했습니다.');
+      alert("시리얼 등록에 실패했습니다.");
     }
   };
 
@@ -33,17 +33,32 @@ function SerialInput() {
     <form onSubmit={handleFormSubmit}>
       <label>
         <h4>S/N</h4>
-        <input type="text" name="dev_serial" value={devSerial} onChange={(e) => setDevSerial(e.target.value)} />
+        <input
+          type="text"
+          name="dev_serial"
+          value={devSerial}
+          onChange={(e) => setDevSerial(e.target.value)}
+        />
       </label>
       <label>
         <h4>점소코드</h4>
-        <input type="text" name="bran_cd" value={branCd} onChange={(e) => setBranCd(e.target.value)} />
+        <input
+          type="text"
+          name="bran_cd"
+          value={branCd}
+          onChange={(e) => setBranCd(e.target.value)}
+        />
       </label>
       <label>
         <h4>업체</h4>
-        <select name="company" value={company} onChange={(e) => setCompany(e.target.value)}>
+        <select
+          name="company"
+          value={company}
+          onChange={(e) => setCompany(e.target.value)}
+        >
           <option value="logen">로젠</option>
           <option value="hangin">한진</option>
+          <option value="lotte">롯데</option>
         </select>
       </label>
       <input id="submitBtn" type="submit" value="시리얼 등록" />
